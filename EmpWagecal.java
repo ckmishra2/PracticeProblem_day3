401685c39087f4ded8c970d5332dc5a791ade9ff
ckmishra2 <chandrakalatiwari2@gmail.com>
2022-09-01 22:23:03 +0530

    Calculate Daily Employee Wage

--- EmpWagecal.java
+++ EmpWagecal.java
@@ -5,12 +5,18 @@ public class EmpWagecal {
 
     public static void main( String[] args )
     {
-    	int fulltime = 1;
+ 	int fulltime = 1;
+    	int empRatePH = 20;
+    	int empHrs = 0;
+    	int empWage = 0;
     	double empcheck = Math.floor(Math.random() * 10)% 2;
     	if (empcheck == fulltime)
-    		System.out.println("Employee is Present");
+    		empHrs = 8;
     	else
-    		System.out.println("Employee is Abscent");
+    		empHrs = 0;
+    	empWage = empHrs*empRatePH;	
+    	
+    		System.out.println("Employee Wage:" + empWage);
       
     }
 

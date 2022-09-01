28b02471e18080e939e0592321c7b1b49c2b25d8
ckmishra2 <chandrakalatiwari2@gmail.com>
2022-09-01 23:28:57 +0530

    Calculating Wages for a Month

--- EmpWagecal.java
+++ EmpWagecal.java
@@ -3,11 +3,12 @@ package basics;
 public class EmpWagecal {
  
 
-    public static void main( String[] args )
-    {
- 		int empRatePH = 20;
+    public static void main( String[] args ){
+		int empRatePH = 20;
     	int empHrs = 0;
     	int empWage = 0;
+    	int totalempWage = 0;
+    	for(int day=0; day<=20; day++) {
     	int empcheck = (int) Math.floor(Math.random() * 10)% 3;
     	switch (empcheck) {
     	case 1:
@@ -19,9 +20,16 @@ public class EmpWagecal {
     	default:
     		empHrs = 0;
     	}
-    	empWage = empHrs*empRatePH;	
     	
-    		System.out.println("Employee Wage:" + empWage);
+    	
+    	empWage = empHrs*empRatePH;
+    	totalempWage += empWage;
+   
+    	System.out.println("Employee Wage:" + empWage);
+    }
+    System.out.println("Toatl Employee Wage: "+ totalempWage);
+          
+
       
     }
 
